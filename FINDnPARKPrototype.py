a21ab458086668a871701eb1382955052d8f3b9f
Rudransh Das <143277515+rd9437@users.noreply.github.com>
2023-11-05 13:16:36 +0530

    Update FINDnPARKPrototype.py

--- FINDnPARKPrototype.py
+++ FINDnPARKPrototype.py
@@ -5,29 +5,20 @@ import pandas as pd
 data = pd.read_csv('parkingdataset.csv')
 
 # Prediction function
-def predict_parking_probability(input_day, input_hour):
+def predict_parking_percentage(input_day, input_hour):
     if input_hour in data.columns and input_day in data['Day'].unique():
         total_slots = data['Total Slots'][data['Day'] == input_day].sum()
         occupied = data[input_hour][data['Day'] == input_day].sum()
         
         if pd.isnull(total_slots) or pd.isnull(occupied):
-            return f"At {input_hour}: No data available to calculate probability."
+            return f"At {input_hour}: No data available to calculate percentage."
         
         probability = 1 - (occupied / total_slots)
-        
-        if probability > 0.8:
-            result_sentence = "This is a good hour to find a parking spot."
-        elif probability > 0.5:
-            result_sentence = "This hour might have moderate occupancy."
-        else:
-            result_sentence = "This is not a good hour to find a parking spot."
         
         # Calculate expected available parking slots as a percentage of the total slots (218) and round to the nearest integer
         expected_available_percentage = int(probability * 218)
         
-        prediction = f"Chances of getting a parking spot at {input_hour}: {probability:.2%}. {result_sentence}"
-        prediction += f" Expected available parking slots as a percentage of the total slots (218): {expected_available_percentage}%."
-        return prediction
+        return f"Expected available parking slots as a percentage of the total slots (218): {expected_available_percentage}%"
     else:
         return "Invalid input. Please provide a valid day and time."
 
@@ -38,5 +29,5 @@ input_day = st.text_input("Enter a day (e.g., Monday):")
 input_hour = st.text_input("Enter the hour (e.g., 8AM - 8PM):")
 
 if st.button("Predict"):
-    prediction = predict_parking_probability(input_day, input_hour)
+    prediction = predict_parking_percentage(input_day, input_hour)
     st.write(prediction)
