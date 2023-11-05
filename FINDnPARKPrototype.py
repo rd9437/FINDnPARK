1d8145f9e9472b55e862a50f25f5183bd4495798
Rudransh Das <143277515+rd9437@users.noreply.github.com>
2023-11-05 12:22:21 +0530

    Update FINDnPARKPrototype.py

--- FINDnPARKPrototype.py
+++ FINDnPARKPrototype.py
@@ -1,11 +1,8 @@
-import streamlit as st
-import pandas as pd
-
 # Load CSV data into a DataFrame
 data = pd.read_csv('parkingdataset.csv')
 
 # Prediction function
-def predict_parking_slots(input_day, input_hour):
+def predict_parking_probability(input_day, input_hour):
     if input_hour in data.columns and input_day in data['Day'].unique():
         total_slots = data['Total Slots'][data['Day'] == input_day].sum()
         occupied = data[input_hour][data['Day'] == input_day].sum()
@@ -13,10 +10,17 @@ def predict_parking_slots(input_day, input_hour):
         if pd.isnull(total_slots) or pd.isnull(occupied):
             return f"At {input_hour}: No data available to calculate probability."
         
-        available_slots = total_slots - occupied
-                
-        slots = f"Chances of getting a parking spot at {input_hour}: {available_slots}"
-        return slots
+        probability = 1 - (occupied / total_slots)
+        
+        if probability > 0.8:
+            result_sentence = "This is a good hour to find a parking spot."
+        elif probability > 0.5:
+            result_sentence = "This hour might have moderate occupancy."
+        else:
+            result_sentence = "This is not a good hour to find a parking spot."
+        
+        prediction = f"Chances of getting a parking spot at {input_hour}: {probability:.2%}. {result_sentence}"
+        return prediction
     else:
         return "Invalid input. Please provide a valid day and time."
 
@@ -26,6 +30,6 @@ st.title("FINDnPARK 1.0")
 input_day = st.text_input("Enter a day (e.g., Monday):")
 input_hour = st.text_input("Enter the hour (e.g., 8AM - 8PM):")
 
-if st.button("Find Space"):
-    slots = predict_parking_slots(input_day, input_hour)
-    st.write(slots)
+if st.button("Predict"):
+    prediction = predict_parking_probability(input_day, input_hour)
+    st.write(prediction)
