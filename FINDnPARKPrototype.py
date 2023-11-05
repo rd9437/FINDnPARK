5cb209cac854cef7df2a66733cddfc1dd366182a
Rudransh Das <143277515+rd9437@users.noreply.github.com>
2023-11-05 12:58:41 +0530

    Update FINDnPARKPrototype.py

--- FINDnPARKPrototype.py
+++ FINDnPARKPrototype.py
@@ -29,6 +29,6 @@ st.title("FINDnPARK 1.0")
 input_day = st.text_input("Enter a day (e.g., Monday):")
 input_hour = st.text_input("Enter the hour (e.g., 8AM - 8PM):")
 
-if st.button("Predict"):
+if st.button("Pret"):
     prediction = predict_parking_probability(input_day, input_hour)
     st.write(prediction)
