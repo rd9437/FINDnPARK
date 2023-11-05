6a4de99536b342e4b6ec5b1c8f03e4ec49928d60
Rudransh Das <143277515+rd9437@users.noreply.github.com>
2023-11-05 12:43:42 +0530

    Update FINDnPARKPrototype.py

--- FINDnPARKPrototype.py
+++ FINDnPARKPrototype.py
@@ -1,3 +1,10 @@
+import streamlit as st
+import pandas as pd
+
+# Load CSV data into a DataFrame
+data = pd.read_csv('parkingdataset.csv')
+
+# Prediction function
 def predict_parking_probability(input_day, input_hour):
     if input_hour in data.columns and input_day in data['Day'].unique():
         total_slots = data['Total Slots'][data['Day'] == input_day].sum()
@@ -22,3 +29,13 @@ def predict_parking_probability(input_day, input_hour):
         return prediction
     else:
         return "Invalid input. Please provide a valid day and time."
+
+# Streamlit UI
+st.title("FINDnPARK 1.0")
+
+input_day = st.text_input("Enter a day (e.g., Monday):")
+input_hour = st.text_input("Enter the hour (e.g., 8AM - 8PM):")
+
+if st.button("Predict"):
+    prediction = predict_parking_probability(input_day, input_hour)
+    st.write(prediction)
