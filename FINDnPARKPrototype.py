ea7f75246e049d5bc0a641ef06d6ff974c1a48e3
Rudransh Das <143277515+rd9437@users.noreply.github.com>
2023-11-05 11:55:50 +0530

    Update FINDnPARKPrototype.py
    To find available number of slots instead of the probability.

--- FINDnPARKPrototype.py
+++ FINDnPARKPrototype.py
@@ -5,7 +5,7 @@ import pandas as pd
 data = pd.read_csv('parkingdataset.csv')
 
 # Prediction function
-def predict_parking_probability(input_day, input_hour):
+def predict_parking_slots(input_day, input_hour):
     if input_hour in data.columns and input_day in data['Day'].unique():
         total_slots = data['Total Slots'][data['Day'] == input_day].sum()
         occupied = data[input_hour][data['Day'] == input_day].sum()
@@ -13,16 +13,9 @@ def predict_parking_probability(input_day, input_hour):
         if pd.isnull(total_slots) or pd.isnull(occupied):
             return f"At {input_hour}: No data available to calculate probability."
         
-        probability = 1 - (occupied / total_slots)
-        
-        if probability > 0.8:
-            result_sentence = "This is a good hour to find a parking spot."
-        elif probability > 0.5:
-            result_sentence = "This hour might have moderate occupancy."
-        else:
-            result_sentence = "This is not a good hour to find a parking spot."
-        
-        prediction = f"Chances of getting a parking spot at {input_hour}: {probability:.2%}. {result_sentence}"
+        available_slots = total_slots - occupied
+                
+        slots = f"Chances of getting a parking spot at {input_hour}: {available_slots}"
         return prediction
     else:
         return "Invalid input. Please provide a valid day and time."
@@ -34,5 +27,5 @@ input_day = st.text_input("Enter a day (e.g., Monday):")
 input_hour = st.text_input("Enter the hour (e.g., 8AM - 8PM):")
 
 if st.button("Predict"):
-    prediction = predict_parking_probability(input_day, input_hour)
+    slots = predict_parking_slots(input_day, input_hour)
     st.write(prediction)
