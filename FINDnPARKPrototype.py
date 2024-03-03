a5c9842a66345081b82d06b8524a09e9237806a7
Rudransh Das <143277515+rd9437@users.noreply.github.com>
2024-03-04 00:11:25 +0530

    Update FINDnPARKPrototype.py

--- FINDnPARKPrototype.py
+++ FINDnPARKPrototype.py
@@ -33,5 +33,6 @@ if st.button("Find Space"):
 
 # Disclaimer message
 st.write("\n")
+st.write("\n")
 st.write("### Disclaimer")
 st.write("This application is part of an ongoing project aimed at analyzing parking space availability using a dataset. Please note that this application is still under construction, and its predictions may not be accurate or reliable. Users are advised not to rely solely on the information provided by this application for their parking decisions. Thank you for your understanding.")
